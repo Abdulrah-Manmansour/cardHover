fc776956fc472d41994fb2c42098adbf0fca9834
Abdulrahman-boop <abdulrahmanmansour600@gmail.com>
2021-04-28 13:00:58 +0200

    first coment

---
+++ main.js
@@ -0,0 +1,53 @@
+// var num1=20;
+// var num2=30;
+// var x="+"; 
+// switch(x){
+//   case "+":
+//     console.log(num1 + num2);
+//     break;
+//     case "-":
+//         console.log(num1 - num2);
+//         break;
+//    case "*":
+//     console.log(num1 * num2);
+//     break;
+//     case "/":
+//         console.log(num1 / num2);
+//     break;
+//     default:
+//         conksole.log("error");     
+// }
+
+
+
+var number = 50;
+ if(number>=90 ){
+    console.log("great");
+ }
+ else if(number>=80 ){
+    console.log("very good");
+ }
+ else if(number>=60 ){
+    console.log("good");
+ }
+ else if(number>=50){
+    console.log("weak")
+ }
+ else{
+    console.log("error");
+ }
+
+var degree = 90;
+switch (degree) {
+    case 90:
+        console.log("great");
+        break;
+    case 80:
+        console.log("very good");
+        break;
+    case 65:
+        console.log("good");
+        break;
+        default:
+         console.log("weak");
+}
